bc49a5ceb6feccfa28cea8fa10565c0990f9a277
Valentin Lanfranchi <valentin.lanfranchi@gmail.com>
2019-03-17 11:17:18 +0100

    Create Network module to provide Retrofit APIs

--- app/src/main/java/com/jehutyno/testhappn/di/component/AppComponent.kt
+++ app/src/main/java/com/jehutyno/testhappn/di/component/AppComponent.kt
@@ -2,11 +2,12 @@ package com.jehutyno.testhappn.di.component
 
 import com.jehutyno.testhappn.TestHappnApp
 import com.jehutyno.testhappn.di.module.AppModule
+import com.jehutyno.testhappn.di.module.NetworkModule
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class])
+@Component(modules = [AppModule::class, NetworkModule::class])
 interface AppComponent {
     fun inject(app: TestHappnApp)
 }
--- app/src/main/java/com/jehutyno/testhappn/di/module/NetworkModule.kt
+++ app/src/main/java/com/jehutyno/testhappn/di/module/NetworkModule.kt
@@ -1,7 +1,32 @@
 package com.jehutyno.testhappn.di.module
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.jehutyno.testhappn.network.ArticlesApi
+import com.jehutyno.testhappn.network.BASE_URL
 import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 class NetworkModule {
+
+    @Provides
+    @Singleton
+    internal fun provideArticlesApi(retrofit: Retrofit): ArticlesApi {
+        return retrofit.create(ArticlesApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
+
+
 }
